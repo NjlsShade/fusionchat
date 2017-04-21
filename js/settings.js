8247dc8085aef1ae25a3c022bc76874e5446fef8
NjlsAssets <njlsassets.com>
2017-04-21 00:13:14 -0400

    OnLoad vs No Wrap

--- js/settings.js
+++ js/settings.js
@@ -6,21 +6,19 @@ var data = fs.readFileSync('data/data.json', 'utf8');
 console.log(data);
 
 function save() {
-	var lang = document.getElementById('mySelect').value;
+	var lang = document.getElementById('settings_lang').value;
 	fs.writeFileSync('test.txt', lang, 'utf8');
 }
 
 
-function settingsSet(){
+function off(){
 	var temp = fs.readFileSync('test.txt', 'utf8');
-	var mySelect = document.getElementById('mySelect');
+	var mySelect = document.getElementById('settings_lang');
 
 	for(var i, j = 0; i = mySelect.options[j]; j++) {
 	    if(i.value == temp) {
 		mySelect.selectedIndex = j;
-		console.log('got here');
 		break;
 	    }
-	    console.log('nope');
 	}
 }
